f185ad37a338c86335769f29ba535c0275a107ea
frank <f.devries@outlook.com>
2020-10-24 19:55:13 +0200

    added Logrocket

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,12 @@
 import "typeface-lato/index.css"
 import "typeface-montserrat/index.css"
 import "./src/scss/main.scss"
+import LogRocket from 'logrocket';
 
 export { wrapPageElement, wrapRootElement } from "./gatsby-ssr"
+
+export const onClientEntry = () => {
+  if (process.env.GATSBY_LOGROCKET === "TRUE") {
+    LogRocket.init('eqfjjs/brunmontagnecom');
+  }
+}
