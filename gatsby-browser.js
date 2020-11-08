c848377fcdd61442878ca2709154a4bd395e83ee
frank <f.devries@outlook.com>
2020-11-08 13:04:35 +0100

    removed auto refresh sw

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -11,4 +11,4 @@ export const onClientEntry = () => {
   }
 }
 
-export const onServiceWorkerUpdateReady = () => window.location.reload(true)
+// export const onServiceWorkerUpdateReady = () => window.location.reload(true)
