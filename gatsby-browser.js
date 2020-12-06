320ad7de13c1ea4d8ca095a06e1928ff39d2431d
frank <f.devries@outlook.com>
2020-12-06 13:15:02 +0100

    removed logrocket

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,14 +1,14 @@
 import "typeface-lato/index.css"
 import "typeface-montserrat/index.css"
 import "./src/scss/main.scss"
-import LogRocket from "logrocket"
+// import LogRocket from "logrocket"
 
 export { wrapPageElement, wrapRootElement } from "./gatsby-ssr"
 
-export const onClientEntry = () => {
-  if (process.env.GATSBY_LOGROCKET === "TRUE") {
-    LogRocket.init("eqfjjs/brunmontagnecom")
-  }
-}
+// export const onClientEntry = () => {
+//   if (process.env.GATSBY_LOGROCKET === "TRUE") {
+//     LogRocket.init("eqfjjs/brunmontagnecom")
+//   }
+// }
 
 // export const onServiceWorkerUpdateReady = () => window.location.reload(true)
