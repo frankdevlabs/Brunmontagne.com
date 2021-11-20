bbd0cb45edf2fac87a4ed3dd3a9188aa6a749297
Frank de Vries <f.devries@outlook.com>
2021-11-20 13:02:46 +0100

    added further functionallity 2021 banner

--- src/components/Header/homeHeader.js
+++ src/components/Header/homeHeader.js
@@ -14,29 +14,39 @@ const HomeHeader = () => {
   return (
     <>
       <div className="header-home__cover">
+        <div className="header-home__promo-poly-modal">
+          <h3 className="promo-font-side">{t("navigation.promo-fall-2021.tagline")}</h3>
+        </div>
         <div className="header-home__text">
           <div className="header-home__text-wrapper">
             <h1 className="heading-1">brunmontagne</h1>
             <p className="heading-tagline">Represents you</p>
+            <h2 className="promo-font">{t("navigation.promo-fall-2021.fall")} <br/> {t("navigation.promo-fall-2021.promotion")}</h2>
+            <Button
+              to="/collection#watches"
+              className="btn btn--secondary btn--cta"
+            >
+              {t("navigation.promo-fall-2021.term")}
+            </Button>
           </div>
-          <div className="header-home__categories columns">
-            <div className="header-home__category column is-full-mobile">
-              <Button
-                to="/collection#watches"
-                className="btn btn--secondary btn--cta"
-              >
-                {t("navigation.watches.term")}
-              </Button>
-            </div>
-            <div className="header-home__category column is-full-mobile">
-              <Button
-                to="/collection#watch-straps"
-                className="btn btn--secondary btn--cta"
-              >
-                {t("navigation.watch-straps.term")}
-              </Button>
-            </div>
-          </div>
+          {/*<div className="header-home__categories columns">*/}
+          {/*  <div className="header-home__category column is-full-mobile">*/}
+          {/*    <Button*/}
+          {/*      to="/collection#watches"*/}
+          {/*      className="btn btn--secondary btn--cta"*/}
+          {/*    >*/}
+          {/*      {t("navigation.watches.term")}*/}
+          {/*    </Button>*/}
+          {/*  </div>*/}
+          {/*  <div className="header-home__category column is-full-mobile">*/}
+          {/*    <Button*/}
+          {/*      to="/collection#watch-straps"*/}
+          {/*      className="btn btn--secondary btn--cta"*/}
+          {/*    >*/}
+          {/*      {t("navigation.watch-straps.term")}*/}
+          {/*    </Button>*/}
+          {/*  </div>*/}
+          {/*</div>*/}
         </div>
         <Slider />
         {/*{isMobile ? <Slider /> : null}*/}
