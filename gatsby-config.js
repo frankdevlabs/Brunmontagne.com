0a48a371cb16dc2e661604a01834f59863a831e5
Frank de Vries <f.devries@outlook.com>
2021-12-03 17:40:55 +0100

    implemented basic localization functionallity

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,5 +25,37 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/locales`,
+        name: `locale`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-react-i18next`,
+      options: {
+        localeJsonSourceName: `locale`, // name given to `gatsby-source-filesystem` plugin.
+        languages: [`nl`, `en`],
+        defaultLanguage: `nl`,
+        // if you are using Helmet, you must include siteUrl, and make sure you add http:https
+        siteUrl: process.env.GATSBY_SITE_URL,
+        // you can pass any i18next options
+        i18nextOptions: {
+          interpolation: {
+            escapeValue: false, // not needed for react as it escapes by default
+          },
+          keySeparator: false,
+          nsSeparator: false,
+        },
+        // To ignore pages, see: https://github.com/microapps/gatsby-plugin-react-i18next/blob/master/example/gatsby-config.js
+        // pages: [
+        //   {
+        //     matchPath: "/ignored-page",
+        //     languages: ["en"],
+        //   },
+        // ],
+      },
+    },
   ].filter(Boolean),
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,33 @@
 import * as React from "react"
+import { useTranslation } from "gatsby-plugin-react-i18next"
 import { graphql } from "gatsby"
 import ProductListing from "../components/product-listing"
 
-const IndexPage = ({ data }) => (
-  <div>
-    <h1>Brunmontagne &mdash; Represents you</h1>
-    <ProductListing products={data?.shopifyCollection?.products} />
-  </div>
-)
+const IndexPage = ({ data }) => {
+  console.log(data)
+  const { t } = useTranslation()
+  return (
+    <div>
+      <h1>{t("title")}</h1>
+      <ProductListing products={data?.collection?.products} />
+    </div>
+  )
+}
 
 export const query = graphql`
-  query {
-    shopifyCollection(handle: { eq: "frontpage" }) {
+  query ($language: String!) {
+    collection: shopifyCollection(handle: { eq: "frontpage" }) {
       products {
         ...ProductCard
+      }
+    }
+    locales: allLocale(filter: { language: { eq: $language } }) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
       }
     }
   }
