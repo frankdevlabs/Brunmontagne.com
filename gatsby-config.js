b633acb6044d2757ba6134f5dfd096ea042e0f2a
Frank de Vries <f.devries@outlook.com>
2022-01-24 15:55:04 +0100

    added a few production required plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -82,5 +82,95 @@ module.exports = {
         // ],
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Brunmontagne`,
+        short_name: `Brunmontagne`,
+        description: `E-commmerce app van Brunmontagne.`,
+        lang: `nl`,
+        display: `standalone`,
+        icon: `src/assets/images/icon.png`,
+        start_url: `/`,
+        background_color: `#f1f1f1`,
+        theme_color: `#130f40`,
+        legacy: true,
+        // cache_busting_mode: "none",
+        localize: [
+          {
+            start_url: `/en/`,
+            lang: `en`,
+            name: `Brunmontagne`,
+            short_name: `Brunmontagne`,
+            description: `E-commmerce app of Brunmontagne.`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        excludes: [
+          `/404`,
+          `/404.html`,
+          `/en/404`,
+          `/en/404.html`,
+          `/offline-plugin-app-shell-fallback`,
+          `/en/terms`,
+          `/terms`,
+          `/en/privacy`,
+          `/privacy`,
+          `/en/returns`,
+          `/returns`,
+        ],
+        query: `
+          {
+            site {
+              siteMetadata {
+                siteUrl
+              }
+            }
+            allSitePage(filter: {context: {i18n: {routed: {eq: false}}}}) {
+              edges {
+                node {
+                  context {
+                    i18n {
+                      defaultLanguage
+                      languages
+                      originalPath
+                    }
+                  }
+                  path
+                }
+              }
+            }
+          }
+        `,
+        serialize: ({ site, allSitePage }) => {
+          return allSitePage.edges.map((edge) => {
+            const { languages, originalPath, defaultLanguage } =
+              edge.node.context.i18n;
+            const { siteUrl } = site.siteMetadata;
+            const url = siteUrl + originalPath;
+            const links = [
+              { lang: defaultLanguage, url },
+              { lang: "x-default", url },
+            ];
+            languages.forEach((lang) => {
+              if (lang === defaultLanguage) return;
+              links.push({ lang, url: `${siteUrl}/${lang}${originalPath}` });
+            });
+            return {
+              url,
+              changefreq: "daily",
+              priority: originalPath === "/" ? 1.0 : 0.7,
+              links,
+            };
+          });
+        },
+      },
+    },
+    `gatsby-plugin-robots-txt`,
+    `gatsby-plugin-netlify`,
   ].filter(Boolean),
 };
